07a0449777c9c8eefafdaa994754ff9a9de0c6b4
Noam Loewenstern <NoamLoewenstern@gmail.com>
2023-04-25 01:22:55 +0300

    cleaned unused files

--- src/backgroundScripts/background.ts
+++ src/backgroundScripts/background.ts
@@ -1,3 +1,8 @@
-// import './context-menu';
+import { onMessage } from 'webext-bridge/background';
+
 console.log('background script loaded');
-export {};
+
+// must run in order to listen to network onMessage between contexts
+onMessage('keep_alive', () => {
+  console.log('[background] keep_alive message');
+});
